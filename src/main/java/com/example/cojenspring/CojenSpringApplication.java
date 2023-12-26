5dd1a9c8089a9c9b87dc830de03ebfd68fca66e8
najoua-elmansouf <najoua.elman01@gmail.com>
2023-12-26 10:56:31 +0100

    first commit

---
+++ src/main/java/com/example/cojenspring/CojenSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cojenspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CojenSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CojenSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cojenspring/CojenSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cojenspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CojenSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
